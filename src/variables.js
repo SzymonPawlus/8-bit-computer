9b062983697ca0eed3230828d2f7772d249e90c1
PiotrekPKP <peterpkppeter@gmail.com>
2021-01-12 21:04:48 +0100

    Better colors

--- src/variables.js
+++ src/variables.js
@@ -20,7 +20,7 @@ export const commands = [
 
 export const coloring = [
     { value: /;.*$/, color: "gray", replace: "$&" },
-    { value: "#define", color: "blue", replace: "#define" },
+    { value: "#define", color: "#5e7fff", replace: "#define" },
     { value: /0[xX][0-9a-fA-F]+/, color: "yellow", replace: "$&" },
     { value: /^\.\w+[a-zA-Z0-9_]:/, color: "green", replace: "$&" },
 ]
