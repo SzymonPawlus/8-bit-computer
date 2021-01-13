30bd072d97df142b383b5cb0ace40d62d9ef837e
SzymonPawlus <56743064+SzymonPawlus@users.noreply.github.com>
2021-01-13 15:08:56 +0100

    Pluged compilator

--- src/fileHandler.js
+++ src/fileHandler.js
@@ -1,6 +1,7 @@
 const dialog = window.require('electron').remote.dialog;
 const { remote } = window.require('electron');
 const fs = window.require('fs');
+const { PythonShell } = window.require("python-shell")
 
 export default class fileHandler {
     static async saveFileAs(data) {
@@ -31,7 +32,14 @@ export default class fileHandler {
 
     static async compileScript(data, file) {
         let file_ = await this.saveFile(data, file);
-        console.log('compile here');
+        let options = {
+            mode: 'text',
+            args: [file, "./src/output/code"],
+        };
+        PythonShell.run("./src/python/compiler.py", options, function (err, results){
+            if(err) console.log(err);
+            console.log(results);
+        })
         return file_;
     }
 }
